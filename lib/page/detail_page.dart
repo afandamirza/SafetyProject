684d22fd665f25501c89a561ff955b5ee1306b69
Angger Ilham <angger.ilhamamanullah004@gmail.com>
2024-08-08 10:31:40 +0700

    feat: add routing page

--- lib/page/detail_page.dart
+++ lib/page/detail_page.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 // ignore: depend_on_referenced_packages
 import 'package:intl/intl.dart';
-
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:share_plus/share_plus.dart'; 
+import 'package:universal_html/html.dart' as html; // 
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/services.dart'; // Import for Clipboard
 
 class DetailPage extends StatelessWidget {
   final DocumentSnapshot documentSnapshot;
+  
 
   const DetailPage({super.key, required this.documentSnapshot});
 
@@ -33,6 +40,30 @@ class DetailPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Detail Page'),
         actions: [
+          IconButton(
+            icon: const Icon(Icons.download),
+            onPressed: () async {
+              if (data['Image'] != null) {
+                await _downloadImage(data['Image'], context);
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("No image to download")),
+                );
+              }
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.share),
+            onPressed: () {
+              _shareContent(context, data);
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.copy),
+            onPressed: () {
+              _copyLinkToClipboard(context);
+            },
+          ),
           IconButton(
             icon: const Icon(Icons.delete),
             onPressed: () {
@@ -87,8 +118,8 @@ class DetailPage extends StatelessWidget {
                       style: TextStyle(
                         fontSize: 16,
                         color: getStatusColor(
-                            data['Safety Report'] ?? ''), // Warna khusus
-                        fontWeight: FontWeight.w500, // Cetak tebal
+                            data['Safety Report'] ?? ''), // Custom color
+                        fontWeight: FontWeight.w500, // Bold
                       ),
                     ),
                   ],
@@ -147,7 +178,88 @@ class DetailPage extends StatelessWidget {
     });
   }
 
-  getStatusColor(String status) {
+  Future<void>  _downloadImage(String url, BuildContext context) async {
+    try {
+      if (kIsWeb) {
+        // Web-specific code to download image
+        var imgReq = html.HttpRequest();
+        imgReq.open('GET', url);
+        imgReq.responseType = 'blob';
+        imgReq.onLoadEnd.listen((e) {
+          final blob = imgReq.response;
+          final url = html.Url.createObjectUrlFromBlob(blob);
+          final anchor = html.AnchorElement(href: url)
+            ..setAttribute("download", "image.png")
+            ..click();
+          html.Url.revokeObjectUrl(url);
+        });
+        imgReq.send();
+      } else {
+        // Mobile-specific code to download image
+        // Get the application documents directory
+        Directory appDocDir = await getApplicationDocumentsDirectory();
+        String appDocPath = appDocDir.path;
+
+        // Create the file path to save the image
+        String fileName = url.split('/').last;
+        String filePath = '$appDocPath/$fileName';
+
+        // Download the image
+        Dio dio = Dio();
+        await dio.download(url, filePath);
+
+        // Show success message
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Image downloaded successfully")),
+        );
+      }
+    } catch (error) {
+      // Show error message
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Failed to download image: $error")),
+      );
+    }
+  }
+
+  void _shareContent(BuildContext context, Map<String, dynamic> data) {
+    String shareText = '''
+      ${documentSnapshot.reference.path}
+      Location: ${data['Location'] ?? 'No Location'}
+      Date: ${data['Time stamp'] != null ? DateFormat('MMMM d, yyyy \'at\' h:mm:ss a').format((data['Time stamp'] as Timestamp).toDate()) : 'No Timestamp'}
+      Safety Report: ${data['Safety Report'] ?? 'No Status'}
+    ''';
+
+    if (kIsWeb) {
+      // Web-specific code to share content
+      html.window.navigator.share({
+        'title': 'Detail Page',
+        'text': shareText,
+        'url': documentSnapshot.reference.path, // Share URL if needed
+      }).catchError((error) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Failed to share content: $error")),
+        );
+      });
+    } else {
+      Share.share(shareText);
+    }
+  }
+
+  void _copyLinkToClipboard(BuildContext context) {
+    Clipboard.setData(
+      ClipboardData(text: documentSnapshot.reference.path),
+    ).then((_) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Link copied to clipboard")),
+      );
+    }).catchError((error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Failed to copy link: $error")),
+      );
+    });
+  }
+
+  Color getStatusColor(String status) {
     switch (status) {
       case "No Googles":
         return Colors.blue;
