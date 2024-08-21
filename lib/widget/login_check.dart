9a51d0631d48cce4d1f3eef699ba367e4941ddd0
afandamirza <115178487+afandamirza@users.noreply.github.com>
2024-08-21 15:09:10 +0700

    Update login_check.dart
    Mengubah  const LoginCheck({super.key, required this.child}); Menjadi   const LoginCheck({Key? key, required this.child}) : super(key: key);

--- lib/widget/login_check.dart
+++ lib/widget/login_check.dart
@@ -4,7 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 class LoginCheck extends StatelessWidget {
   final Widget child;
 
-  const LoginCheck({super.key, required this.child});
+  const LoginCheck({Key? key, required this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
